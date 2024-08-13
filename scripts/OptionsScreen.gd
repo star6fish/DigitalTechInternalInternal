23ec857ed29364d38f73a93c46adbaccdff25691
star6fish <jjansen@students.mags.school.nz>
2024-08-13 21:58:02 +1200

    12/08/2024

--- scripts/OptionsScreen.gd
+++ scripts/OptionsScreen.gd
@@ -1,5 +1,19 @@
 extends Control
 
+@onready var global = get_node("res://scripts/global.gd")
+
+func _grey():
+	global.ColourPlane = Color.GRAY
+	
+func _green():
+		global.ColourPlane = Color.GREEN
+	
+func _blue():
+		global.ColourPlane = Color.BLUE
+
+func  _orange():
+		global.ColourPlane = Color.ORANGE
+
 func _back():
 	get_tree().change_scene_to_file("res://scenes/PlayScreen.tscn")
 
---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var planeColour = Color.GRAY
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
